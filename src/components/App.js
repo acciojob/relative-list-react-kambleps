b8063434ce32460be2da9d32fb583c514eb63fef
kambleps <125243027+kambleps@users.noreply.github.com>
2023-06-27 10:36:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,13 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+    <ol key="relativeList">
+      <li key="relativeListItem1">Aunt Jane</li>
+      <li key="relativeListItem2">Uncle Mike</li>
+      <li key="relativeListItem3">Cousin Sarah</li>
+      <li key="relativeListItem4">Grandma Rose</li>
+      <li key="relativeListItem5">Brother Alex</li>
+    </ol>
     </div>
   )
 }
