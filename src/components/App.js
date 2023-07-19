490a99a9634f073112421c33661bdff985359a28
kambleps <125243027+kambleps@users.noreply.github.com>
2023-07-19 06:00:29 +0000

    two

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,12 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
-    <ol key="relativeList">
-      <li key="relativeListItem1">Aunt Jane</li>
-      <li key="relativeListItem2">Uncle Mike</li>
-      <li key="relativeListItem3">Cousin Sarah</li>
-      <li key="relativeListItem4">Grandma Rose</li>
-      <li key="relativeListItem5">Brother Alex</li>
+    <ol key="relativeList" id='relativeList'>
+      <li key="relativeListItem1" id='relativeListItem1'>Aunt Jane</li>
+      <li key="relativeListItem2" id='relativeListItem2'>Uncle Mike</li>
+      <li key="relativeListItem3" id='relativeListItem3'>Cousin Sarah</li>
+      <li key="relativeListItem4" id='relativeListItem4'>Grandma Rose</li>
+      <li key="relativeListItem5" id='relativeListItem5'>Brother Alex</li>
     </ol>
     </div>
   )
